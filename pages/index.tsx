757a531f8fba6ace81cb9db2f5894a17227e1e98
Quantum Snowball <quantum.snowball@gmail.com>
2023-02-07 00:17:13 +0800

    feat: some title and desc text

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,25 +39,10 @@ export default function Home() {
           </div>
         </div>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
+        <h1>Welcome to TrBox Console</h1>
+        <h2>Hello World!</h2>
+        <h2>This is the all-in-one algo trading dashboard you ever need.</h2>
+
 
         <div className={styles.grid}>
           <a
