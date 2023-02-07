82d78b53d1a56243de1415bf24929446202758f2
Quantum Snowball <quantum.snowball@gmail.com>
2023-02-07 13:01:20 +0800

    layout: responsive layout

--- components/MenuBar/index.tsx
+++ components/MenuBar/index.tsx
@@ -1,6 +1,8 @@
 import { AppBar, IconButton, Toolbar, Typography } from "@mui/material"
 import MenuIcon from '@mui/icons-material/Menu'
 import Link from "next/link"
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 
 const PageLink = ({ title, href }: { title: string, href: string }) =>
@@ -15,18 +17,26 @@ const PageLink = ({ title, href }: { title: string, href: string }) =>
 
 
 const MenuBar = () => {
+  const theme = useTheme();
+  const isSmall = useMediaQuery(theme.breakpoints.down('sm'));
+
   return (
     <AppBar>
       <Toolbar>
         <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
           TRBOX
         </Typography>
-        <PageLink title='Home' href='/' />
-        <PageLink title='Navs' href='/' />
-        <PageLink title='Trade Log' href='/' />
-        <IconButton edge="start" color="inherit" aria-label="menu">
-          <MenuIcon />
-        </IconButton>
+        {isSmall ?
+          <IconButton color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          :
+          <>
+            <PageLink title='Home' href='/' />
+            <PageLink title='Navs' href='/' />
+            <PageLink title='Trade Log' href='/' />
+          </>
+        }
       </Toolbar>
     </AppBar>
   )
