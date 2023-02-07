259e599a73b91c92378e8846297cf443a339ac82
Quantum Snowball <quantum.snowball@gmail.com>
2023-02-07 12:54:14 +0800

    layout: basic navbar links

--- components/MenuBar/index.tsx
+++ components/MenuBar/index.tsx
@@ -1,9 +1,32 @@
-import { AppBar, Toolbar } from "@mui/material"
+import { AppBar, IconButton, Toolbar, Typography } from "@mui/material"
+import MenuIcon from '@mui/icons-material/Menu'
+import Link from "next/link"
+
+
+const PageLink = ({ title, href }: { title: string, href: string }) =>
+  <Typography
+    variant='h6'
+    sx={{
+      mx: 1
+    }}
+  >
+    <Link href={href}>{title}</Link>
+  </Typography>
+
 
 const MenuBar = () => {
   return (
     <AppBar>
       <Toolbar>
+        <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
+          TRBOX
+        </Typography>
+        <PageLink title='Home' href='/' />
+        <PageLink title='Navs' href='/' />
+        <PageLink title='Trade Log' href='/' />
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
       </Toolbar>
     </AppBar>
   )
