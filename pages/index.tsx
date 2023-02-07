983c30a57cd776dc9376421de59071548082bb6e
Quantum Snowball <quantum.snowball@gmail.com>
2023-02-07 12:21:58 +0800

    style: centered div

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,14 @@ import { styled } from '@mui/material'
 
 
 const Div = styled('div')`
+  /* take all vertical space */
+  height: 100%;
+  /* single item each row */
   display: flex;
   flex-flow: column nowrap;
-  justify-content: space-between;
+  /* align vertically */
+  justify-content: center;
+  /* align horizontally */
   align-items: center;
 `;
 
