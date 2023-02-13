5ff37a4deaf242eb44b8a30eaf6f89b6485389b6
Quantum Snowball <quantum.snowball@gmail.com>
2023-02-13 14:43:39 +0800

    rft: useRef for socket

--- components/tradelog/table.tsx
+++ components/tradelog/table.tsx
@@ -17,51 +17,58 @@ import { useRouter } from 'next/router';
 export default function TradeLogTable() {
   const router = useRouter()
   const dispatch = useDispatch()
-  const [socket, setSocket] = useState<WebSocket | null>(null)
-  const socketRef = useRef(socket)
+  const socket = useRef(null as WebSocket | null)
   const [tradelog, addOrderResult, clearTradelog] = [
     useSelector((s: RootState) => s.contentTemp.tradelog),
     (r: OrderResult) => dispatch(contentTempActions.addOrderResult(r)),
     () => dispatch(contentTempActions.clearTradelog()),
   ]
 
-  // reference the latest socket state
-  socketRef.current = socket
-
   // when page enter
   useEffect(() => {
-    // when enter page /tradelog
-    if (router.pathname === '/tradelog') {
-      setSocket(() => new WebSocket(`ws://${window.location.hostname}:${PORT_WS}`))
-    }
     // when page navigate away
     const on_routeChange = (url: string) => {
       // only when leave from /tradelog
-      socketRef.current?.close()
-      clearTradelog()
+      if (url !== '/tradelog') {
+        socket.current?.close()
+        console.debug('ws disconnected')
+        clearTradelog()
+      }
     }
 
-    // when comp mounted
-    router.events.on('routeChangeStart', on_routeChange)
+    // when enter page /tradelog
+    if (router.pathname === '/tradelog') {
+      // connect to socket if there is not already a connection
+      if (!socket.current) {
+        socket.current = new WebSocket(`ws://${window.location.hostname}:${PORT_WS}`)
+        console.debug('ws connected')
+        socket.current.onclose = () => console.debug('onclose: closing connection')
+        // start to listen to message 
+        socket.current.addEventListener('message', (e: MessageEvent<string>) => {
+          const msg: WebSocketMessage = JSON.parse(e.data)
+          if (msg.tag === 'OrderResult') {
+            const { data: orderResult } = msg as TaggedMessage<OrderResult>
+            addOrderResult(orderResult)
+            console.debug(msg.tag)
+          }
+        })
+        // when comp mounted
+        router.events.on('routeChangeStart', on_routeChange)
+        console.debug('monitoring page leave')
+      } else {
+        console.debug('using existing socket')
+      }
+    }
 
     // when comp unmounted
     return () => {
       // when app exit
-      socketRef.current?.close()
-      router.events.off('routeChangeStart', on_routeChange)
+      // socketRef.current?.close()
+      // router.events.off('routeChangeStart', on_routeChange)
+      // BUG these cause problem when in react strict mode
+      console.debug('disconnected on comp unmounted')
     }
   }, [])
-
-  // register handlers if connected
-  useEffect(() => {
-    socket?.addEventListener('message', (e: MessageEvent<string>) => {
-      const msg: WebSocketMessage = JSON.parse(e.data)
-      if (msg.tag === 'OrderResult') {
-        const { data: orderResult } = msg as TaggedMessage<OrderResult>
-        addOrderResult(orderResult)
-      }
-    })
-  }, [socket])
 
   return (
     <TableContainer component={Paper}>
